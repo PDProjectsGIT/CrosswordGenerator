e045d44c3460bb1559031a9e56754c032f6db5e1
PDProjectsGIT <p.drzazga01@gmail.com>
2024-04-26 10:06:45 +0200

    Added new Javadoc comments. DynamicArray file.

--- src/main/java/org/project/model/crossword/structures/DynamicArray.java
+++ src/main/java/org/project/model/crossword/structures/DynamicArray.java
@@ -7,29 +7,57 @@ import java.util.Iterator;
 import java.util.Objects;
 import java.util.Optional;
 
+/**
+ * Class that represents adjustable structure.
+ * It's main feature is to insert new values even in non-existing index.
+ * @param <T>
+ */
 public class DynamicArray<T> implements Iterable<T>{
 
+    /**
+     * An array to store elements of type T. Main data container.
+     */
     protected T[] data;
 
+    /**
+     * Constructs a new instance of the DynamicArray class with the default size of 0.
+     */
     @SuppressWarnings("unchecked")
     public DynamicArray(){
         data = (T[]) new Object[0];
     }
 
+    /**
+     * Constructs a new instance of the DynamicArray class with specified size.
+     * @param size A specified size of new DynamicArray object.
+     */
     @SuppressWarnings("unchecked")
     public DynamicArray(int size){
         data = (T[]) new Object[size];
     }
 
+    /**
+     * Constructs a new instance of the DynamicArray class initialized with elements from another DynamicArray object.
+     * @param dynamicArray Another DynamicArray object whose elements will be copied.
+     */
     public DynamicArray(DynamicArray<T> dynamicArray){
         data = dynamicArray.data.clone();
     }
 
+    /**
+     * Returns an iterator over the elements in this DynamicArray in proper sequence.
+     * @return An iterator over the elements in this DynamicArray in proper sequence.
+     */
     @Override
     public @NotNull Iterator<T> iterator() {
         return new ArrayIterator();
     }
 
+    /**
+     * Indicates whether some other object is "equal to" this DynamicArray.
+     * @param o The reference object with which to compare.
+     * @return True if this DynamicArray is the same as the object argument, false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -38,29 +66,55 @@ public class DynamicArray<T> implements Iterable<T>{
         return Objects.deepEquals(data, that.data);
     }
 
+    /**
+     * Returns the hash code value for this DynamicArray.
+     * @return The hash code value for this DynamicArray.
+     */
     @Override
     public int hashCode() {
         return Arrays.hashCode(data);
     }
 
+    /**
+     * Inner class implementing the Iterator interface for iterating over elements in the DynamicArray.
+     */
     private class ArrayIterator implements Iterator<T> {
         private int currentIndex = 0;
 
+        /**
+         * Returns true if the iteration has more elements.
+         * @return True if the iteration has more elements, false otherwise.
+         */
         @Override
         public boolean hasNext() {
             return currentIndex < data.length;
         }
 
+        /**
+         * Returns the next element in the iteration.
+         * @return The next element in the iteration.
+         */
         @Override
         public T next() {
             return data[currentIndex++];
         }
     }
 
+    /**
+     * Sets the value at the specified index.
+     * @param index The index at which to set the value.
+     * @param value The value to be stored at the specified index.
+     */
     public void setValue(int index, T value){
         data[index] = value;
     }
 
+    /**
+     * Sets the value at the specified index. If the index exceeds the current size of the DynamicArray,
+     * the DynamicArray will resize to accommodate the new value.
+     * @param index The index at which to set the value.
+     * @param value The value to be stored at the specified index.
+     */
     @SuppressWarnings("unchecked")
     final public void setValueOutOfBounds(int index, T value){
         if(index < data.length && index >= 0){
@@ -86,15 +140,29 @@ public class DynamicArray<T> implements Iterable<T>{
         data[index + shift] = value;
     }
 
+    /**
+     * Retrieves the value at the specified index.
+     * @param index The index from which to retrieve the value.
+     * @return The value at the specified index.
+     */
     public Optional<T> getValue(int index)
     {
         return Optional.ofNullable(data[index]);
     }
 
+    /**
+     * Retrieves the current size of the DynamicArray.
+     * @return the number of elements currently stored in the DynamicArray.
+     */
     public int getSize(){
         return data.length;
     }
 
+    /**
+     * Checks whether the DynamicArray contains the specified object.
+     * @param value The object to be checked for presence in the DynamicArray.
+     * @return True if the DynamicArray contains the specified object, otherwise false.
+     */
     public boolean contains(T value){
         if(value == null) throw new NullPointerException("Cannot invoke \"DynamicArray.contains(Object)\" because \"value\" is null");
         for (T val : data) {
---
+++ src/main/java/org/project/model/crossword/structures/package-info.java
@@ -0,0 +1,1 @@
+package org.project.model.crossword.structures;
